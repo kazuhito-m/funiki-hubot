7a7be5473474411589ee179f1ab5464f995e1554
kazuhito-m <kazuhito.sumpic@gmail.com>
2015-01-02 17:09:16 +0900

    イニシャルデータを変更。人より文の内容を重視するように。

--- import_initial_data.sh
+++ import_initial_data.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo 'exec import !'
-cat initial_data.txt  | perl -pe 's/\n$/\r\n/g' |redis-cli --pipe
+cat initial_data_syobochim.txt  | perl -pe 's/\n$/\r\n/g' |redis-cli --pipe
 echo 'import end.'
